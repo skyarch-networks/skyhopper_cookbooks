0cb674ead0f112eed3617ac39334c3db0448a5d8
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-04-24 16:11:40 +0900

    install as ec2-user

--- cookbooks/skyhopper/attributes/defaults.rb
+++ cookbooks/skyhopper/attributes/defaults.rb
@@ -1,4 +1,4 @@
 attr = default['skyhopper']
-attr['root_dir'] = '/root/skyhopper'
+attr['root_dir'] = '/home/ec2-user/skyhopper'
 attr['repo_url'] = 'https://github.com/skyarch-networks/skyhopper.git'
 attr['server_name'] = 'skyhopper.local'
