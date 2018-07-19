15372b8ef62a63113111a9a5e4761cf76c8366e4
Ryo Tanaka <tanaka@skyarch.net>
2018-07-19 16:10:44 +0900

    WIP.

--- cookbooks/zabbix/recipes/agent.rb
+++ cookbooks/zabbix/recipes/agent.rb
@@ -11,7 +11,8 @@ template "zabbix_agentd.conf" do
     group "root"
     mode "644"
   end
-  notifies :restart, "service[#{node['zabbix']['agent']['service_name']}]"
+  #notifies :restart, "service[#{node['zabbix']['agent']['service_name']}]"
+  subscribes :restart, template
 end
 
 ruby_block "start service" do
