6f657b4a11af54d4bdddd25be07c30674c6e4483
Ryo Tanaka <tanaka@skyarch.net>
2018-07-19 11:31:56 +0900

    Fix install_method.

--- cookbooks/zabbix/recipes/agent.rb
+++ cookbooks/zabbix/recipes/agent.rb
@@ -1,4 +1,4 @@
-include_recipe "zabbix::agent_#{node['zabbix']['agent']['install_method']}"
+#include_recipe "zabbix::agent_#{node['zabbix']['agent']['install_method']}"
 include_recipe "zabbix::agent_common"
 
 # Install configuration
